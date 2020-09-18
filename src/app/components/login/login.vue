f768bba2ab99327e027f363d374350feeb40ce84
carlos-lity <bertarinijuniorcarlos@gmail.com>
2020-09-18 16:30:09 -0300

    inicio layout login

--- src/app/components/login/login.vue
+++ src/app/components/login/login.vue
@@ -1,13 +1,56 @@
 <template>
-  <p>login</p>
+  <a-row class="arow--center">
+    <a-col :span="6">
+      <a-form layout="vertical" >
+        <a-form-item>
+          <a-input 
+            v-decorator="[
+              'userName',
+              { rules: [{ required: true, message: 'Please input your username!' }] },
+            ]"
+            placeholder="Username"
+          />
+        </a-form-item>
+        <a-form-item>
+          <a-input
+            v-decorator="[
+              'password',
+              { rules: [{ required: true, message: 'Please input your Password!' }] },
+            ]"
+            type="password"
+            placeholder="Password"
+          >
+            <a-icon slot="prefix" type="lock" style="color:rgba(0,0,0,.25)" />
+          </a-input>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" html-type="submit" >
+            Log in
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
 export default {
-    name: 'login'
+    name: 'login',
+    data: () => ({
+
+    }),
+    methods: {
+      
+    }
 }
 </script>
 
-<style>
-
+<style scoped>
+.arow--center {
+  height: 100vh;
+  display: flex !important;
+  flex: auto !important;
+  align-items: center;
+  justify-content: center;
+}
 </style>
